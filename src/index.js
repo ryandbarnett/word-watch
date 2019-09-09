502e430df160bd12798facbdaccd329833569b5f
RyanDBarnett <conner2468@gmail.com>
2019-09-09 09:55:25 -0600

    Move helper functions outside of document ready

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
   $.get( "https://wordwatch-api.herokuapp.com/api/v1/top_word", (data) => {
     const topWord = Object.keys(data.word)[0];
     const count = data.word[topWord];
-    $('.top-word h3').text(`Top word from Word Watch API: ${capitalize(topWord)} ${count}`);
+    updateTopWord(topWord, count);
   });
 
 
@@ -14,16 +14,20 @@ $(document).ready(() => {
     breakDown(words);
   });
 
-  const capitalize = (word) => {
-    return word.substr(0,1).toUpperCase()+ word.substr(1);
-  }
-
-  const breakDown = (words) => {
-    words.split(' ').forEach(word => postWord(word));
-  }
-
-  const postWord = (word) => {
-    $.post("https://wordwatch-api.herokuapp.com/api/v1/words", { word: { value: word } });
-  }
-
 });
+
+const updateTopWord = (word, count) => {
+  $('.top-word h3').text(`Top word from Word Watch API: ${capitalize(word)} ${count}`);
+}
+
+const capitalize = (word) => {
+  return word.substr(0,1).toUpperCase()+ word.substr(1);
+}
+
+const breakDown = (words) => {
+  words.split(' ').forEach(word => postWord(word));
+}
+
+const postWord = (word) => {
+  $.post("https://wordwatch-api.herokuapp.com/api/v1/words", { word: { value: word } });
+}
