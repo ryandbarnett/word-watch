a9b09ef85f4381965b2f13c1fecfa36bce7fc1ac
RyanDBarnett <conner2468@gmail.com>
2019-09-09 08:16:39 -0600

    Make top word and count display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
 $(document).ready(() => {
-  // have fun!
-})
+  $.get( "https://wordwatch-api.herokuapp.com/api/v1/top_word", (data) => {
+    let topWord = Object.keys(data.word)[0];
+    let count = data.word.vincerulz;
+    topWord = topWord.substr(0,1).toUpperCase()+ topWord.substr(1);
+    $('.top-word h3').text(`Top word from Word Watch API: ${topWord} ${count}`);
+  });
+});
